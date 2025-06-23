aa9c395c68d24efac37ed4d165941aac3b36e9c1
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-06-23 15:20:12 +0530

    add hover on marquee component

--- src/components/ListSlider.jsx
+++ src/components/ListSlider.jsx
@@ -9,7 +9,7 @@ const ListSlider = () => {
                 <Marquee>
                     <div className="flex gap-3">
                         {LISTSLIDER_DATA.map((list, index) => (
-                            <div key={index} className="p-4 bg-white border border-[#112D491A] rounded-[8px] !max-w-[207px] min-w-[207px] w-full flex items-center gap-3.5">
+                            <div key={index} className="p-4 transition-all duration-200 ease-linear cursor-pointer border hover:shadow-[0px_2px_20px_0px_#112D491A] border-[#112D491A] rounded-[8px] !max-w-[207px] min-w-[207px] w-full flex items-center gap-3.5">
                                 <div className="max-w-[175px] w-full flex items-center gap-3.5">
                                     <div className="min-w-[57px] min-h-[57px] bg-[#112D49] rounded-[124px] flex justify-center items-center"><list.icon /></div>
                                     <div className="font-bold leading-[120%] text-[#112D49">{list.title}</div>
